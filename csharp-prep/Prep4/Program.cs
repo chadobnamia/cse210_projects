b9a1768e1257be0b0a51f2847acb24fddcf508ca
ChadDev2022 <richardobnamia@gmail.com>
2024-01-14 04:39:37 +0800

    Added list practice code

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbersList = new List<int>();
+        
+        int userInput = -1;
+        while (userInput != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            
+            string userResponse = Console.ReadLine();
+            userInput = int.Parse(userResponse);
+            
+            
+            if (userInput != 0)
+            {
+                numbersList.Add(userInput);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbersList)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+
+        float average = ((float)sum) / numbersList.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        
+        int max = numbersList[0];
+
+        foreach (int number in numbersList)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
