6b11a7445d5a364c96cf57d1d826a89a8d3066cb
ChadDev2022 <richardobnamia@gmail.com>
2024-01-23 09:17:50 +0800

    added journal program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Please select one of the follwing choices.");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+
+        
+
+        
     }
 }
--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,4 +1,9 @@
 public class PromptGenerator
 {
-    
+    public List<string> _prompt;
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
 }
